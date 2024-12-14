68f2a83700a93536321de3e68367b81e00f164fe
Andrew2797 <andrijgolub27@gmail.com>
2024-12-14 11:32:02 +0200

    plus option

--- main.py
+++ main.py
@@ -1,32 +1,55 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 from data import data
+from data.base import create_db
+#from data.data_to_db import write_data_to_db
+from data.base import Session
+from data.models import Reserve, Tour
 
 
 app = Flask(__name__)
-NAVIGATION = data.departures
+
+
+@app.context_processor
+def global_data():
+    return dict(
+        departures=data.departures
+    )
 
 
 @app.get("/")
 def index():
-    return render_template("index.html", departures=NAVIGATION, tours=data.tours)
+    with Session() as session:
+        tours = session.query(Tour).all()
+        return render_template("index.html", tours=tours)
 
 
 @app.get("/tour/<int:id>")
 def get_tour(id):
-    tour = data.tours.get(id)
-    return render_template("tour.html", departures=NAVIGATION, tour=tour)
+    with Session() as session:
+        tour = session.query(Tour).where(Tour, id == id).first()
+        return render_template("tour.html", tour=tour)
 
 
 @app.get("/departure/<dep_eng>")
 def departure(dep_eng):
-    tours = {}
-    for id, tour in data.tours.items():
-        if tour["departure"] ==dep_eng:
-            tours[id] = tour
-    
-    return render_template("departure.html", departures=NAVIGATION, tours=tours)
+    with Session() as session:
+        tours = session.query(Tour).where(Tour.departure == dep_eng).all()
+
+        return render_template("departure.html", tours=tours, dep_eng=dep_eng)
+
+
+@app.post("/tour/reserve/<int:id>")
+def reserve(id):
+    with Session() as session:
+        #name = request.form.get("name")
+        #reserve_tour = Reserve(name=name, tour_id=id)
+        #session.add(reserve_tour)
+        #session.commit()
+        return redirect(url_for("index"))
 
 
 if __name__ == "__main__":
+    create_db()
+    #write_data_to_db()
     app.run(debug=True)
